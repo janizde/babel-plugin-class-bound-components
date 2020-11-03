65f335a38e593e984dc6d12f00096e6dfb971e0a
Jannik Portz <jannik.portz@soundcloud.com>
2020-11-03 16:20:48 +0100

    rename imported name of extend in test fixture

--- test/fixtures/display-name-extend/actual.js
+++ test/fixtures/display-name-extend/actual.js
@@ -1,7 +1,7 @@
-import classBound, { extend } from 'class-bound-components';
+import classBound, { extend as someOtherLocalSymbol } from 'class-bound-components';
 
 const FooComponent = classBound('fooClass');
 const BarComponent1 = classBound.extend(FooComponent, 'barClass');
-const BarComponent2 = extend(FooComponent, 'barClass');
+const BarComponent2 = someOtherLocalSymbol(FooComponent, 'barClass');
 const BarComponent3 = classBound.extend(FooComponent, 'barClass', 'BazComponent', { bar: 'barClass' });
 const BarComponent4 = classBound.extend(FooComponent, 'barClass', { bar: 'barClass' });
--- test/fixtures/display-name-extend/expected.js
+++ test/fixtures/display-name-extend/expected.js
@@ -1,11 +1,17 @@
-import classBound, { extend } from 'class-bound-components';
+import classBound, {
+  extend as someOtherLocalSymbol,
+} from 'class-bound-components';
 const FooComponent = classBound('fooClass', 'FooComponent');
 const BarComponent1 = classBound.extend(
   FooComponent,
   'barClass',
   'BarComponent1'
 );
-const BarComponent2 = extend(FooComponent, 'barClass', 'BarComponent2');
+const BarComponent2 = someOtherLocalSymbol(
+  FooComponent,
+  'barClass',
+  'BarComponent2'
+);
 const BarComponent3 = classBound.extend(
   FooComponent,
   'barClass',
